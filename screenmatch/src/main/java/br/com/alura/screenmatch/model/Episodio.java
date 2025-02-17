57f9af2fd0a2b5b9de725f68c77dcb0a22b8ede1
AndersonDalMagro <dalmagro299@gmail.com>
2025-02-17 15:22:46 -0300

    pare 3 do projeto

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,78 @@
+package br.com.alura.screenmatch.model;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodios;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Integer getTemporda() {
+        return temporada;
+    }
+
+    public void setTemporda(Integer temporda) {
+        this.temporada = temporda;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodios() {
+        return numeroEpisodios;
+    }
+
+    public void setNumeroEpisodios(Integer numeroEpisodios) {
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodios = dadosEpisodio.numero();
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        }catch (NumberFormatException ex){
+            this.avaliacao = 0.0;
+        }
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeParseException ex){
+            this.dataLancamento = null;
+        }
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodios=" + numeroEpisodios +
+                ", avaliacao=" + avaliacao +
+                ", dataLancamento=" + dataLancamento ;
+    }
+}
